a6dd0ef59535940199dab82ef75eadbba103bda7
Jorge Cano <coronero@gmail.com>
2023-03-12 09:00:09 -0700

    104-Assignment2

--- Script/displayInfo.js
+++ Script/displayInfo.js
@@ -1,4 +1,7 @@
-document.getElementById('pets').innerHTML = `<h1 class="counter">We currently have ${petSalon.pets.length} pets with us.</h1>`;
+
+document.getElementById.innerHTML("petsDisplay").innerHTML = `<h1 class="counter">We currently have ${petSalon.pets.length} pets with us.</h1>`;
+
+console.log(petSalon.pets.length);
 
 function displayLoc() {
     alert(`${petSalon.address.street}, ${petSalon.address.city} ${petSalon.address.zip}`);
@@ -8,72 +11,23 @@ function displayHours() {
 }
 
 function displayInfo() {
+    //printing dog names to console
     for(let i = 0; i < petSalon.pets.length; i++){
         console.log(petSalon.pets[i].name);
     }
-    document.getElementById("guests1").innerHTML = `<div class=dogInfo">
-    <img src="img/doggy.png" alt="dog-pic" width=150 height=100>
-    <ul>
-    <li>My name is <b>${petSalon.pets[0].name}</b></li>
-    <li>I am ${petSalon.pets[0].age} years old.</li>
-    <li>I am a ${petSalon.pets[0].gender}</li>
-    <li>I'm here for ${petSalon.pets[0].service}</li>
-    </ul>
-    </div>`;
-
-    document.getElementById("guests2").innerHTML = `<div class=dogInfo">
-    <img src="img/doggy.png" alt="dog-pic" width=150 height=100>
-    <ul>
-    <li>My name is <b>${petSalon.pets[1].name}</b></li>
-    <li>I am ${petSalon.pets[1].age} years old.</li>
-    <li>I am a ${petSalon.pets[1].gender}</li>
-    <li>I'm here for ${petSalon.pets[1].service}</li>
-    </ul>
-    </div>`;
-
-    document.getElementById("guests3").innerHTML = `<div class=dogInfo">
-    <img src="img/doggy.png" alt="dog-pic" width=150 height=100>
-    <ul>
-    <li>My name is <b>${petSalon.pets[2].name}</b></li>
-    <li>I am ${petSalon.pets[2].age} years old.</li>
-    <li>I am a ${petSalon.pets[2].gender}</li>
-    <li>I'm here for ${petSalon.pets[2].service}</li>
-    </ul>
-    </div>`;
-}
-
-function displayInfo2() {
+    //printing info on all dogs to the page.
     for(let i = 0; i < petSalon.pets.length; i++){
-        console.log(petSalon.pets[i].name);
+        document.getElementById("info").innerHTML = `<div class="dogInfo">
+            <img src="img/doggy.png" alt="dog-pic" width=150 height=100>
+            <ul>
+                <li>My name is <b>${petSalon.pets[i].name}</b></li>
+                <li>I am ${petSalon.pets[i].age} years old.</li>
+                <li>I am a ${petSalon.pets[i].gender}</li>
+                <li>I'm here for ${petSalon.pets[i].service}</li>
+                <li>My human's name is ${petSalon.pets[i].owner}</li>
+                <li>Our contact number is ${petSalon.pets[i].phone}</li>
+            </ul>
+        </div>`;
     }
-    document.getElementById("info").innerHTML= `<h4>Too bad!</h4>`;
-    document.getElementById("guests1").innerHTML = `<div class=dogInfo">
-    <img src="img/doggy.png" alt="dog-pic" width=150 height=100>
-    <ul>
-    <li>My name is ${petSalon.pets[0].name}</li>
-    <li>I am ${petSalon.pets[0].age} years old.</li>
-    <li>I am a ${petSalon.pets[0].gender}</li>
-    <li>I'm here for ${petSalon.pets[0].service}</li>
-    </ul>
-    </div>`;
-
-    document.getElementById("guests2").innerHTML = `<div class=dogInfo">
-    <img src="img/doggy.png" alt="dog-pic" width=150 height=100>
-    <ul>
-    <li>My name is ${petSalon.pets[1].name}</li>
-    <li>I am ${petSalon.pets[1].age} years old.</li>
-    <li>I am a ${petSalon.pets[1].gender}</li>
-    <li>I'm here for ${petSalon.pets[1].service}</li>
-    </ul>
-    </div>`;
-
-    document.getElementById("guests3").innerHTML = `<div class=dogInfo">
-    <img src="img/doggy.png" alt="dog-pic" width=150 height=100>
-    <ul>
-    <li>My name is ${petSalon.pets[2].name}</li>
-    <li>I am ${petSalon.pets[2].age} years old.</li>
-    <li>I am a ${petSalon.pets[2].gender}</li>
-    <li>I'm here for ${petSalon.pets[2].service}</li>
-    </ul>
-    </div>`;
+    
 }
--- Script/register.js
+++ Script/register.js
@@ -12,27 +12,63 @@ let petSalon = {
         close: "7:00 pm"
     },
     pets:[
-        {
-            name: "Max",
-            age: 12,
-            gender: "Male",
-            service: "Pampering"
-        },
-        {
-            name: "Porky",
-            age: .5,
-            gender: "Female",
-            service: "Grooming"
-        },
-        {
-            name: "Jonathan",
-            age: 11,
-            gender: "Male",
-            service: "Nail Trimming"
-        }
 
     ]
 }
+
+//object constructor
+function Pet(n, a, g, s, o, p) {
+    this.name = n;
+    this.age = a;
+    this.gender = g;
+    this.services = s;
+    this.owner = o;
+    this.phone = p;
+
+}
+
+ //get the values from the inputs
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputService = document.getElementById("txtService");
+let inputOwner = document.getElementById("txtOwner");
+let inputPhone = document.getElementById("txtPhone");
+
+//register function
+function register() {
+    console.log("registered");
+    //create the newPet
+    let newPet = new Pet(inputName.value, inputAge.value, inputGender.value, inputService.value, inputOwner.value, inputPhone.value);
+
+    console.log(newPet);
+    //push the newPet obj into the array
+    petSalon.pets.push(newPet);
+    //display the pets into the array
+    console.log(petSalon.pets);
+    //clear the form
+    clearForm();
+}
+
+//clearForm function
+function clearForm(){
+    inputName.value="";
+    inputAge.value="";
+    inputGender.value="";
+    inputService.value="";
+    inputOwner.value="";
+    inputPhone.value="";
+}
+
+//init function
+function init(){
+    console.log("init");
+    let pet1 = new Pet("Scooby", 60, "Male", "Grooming", "Shaggy", 6197795656);
+    let pet2 = new Pet("Scrappy", 200, "Male", "Pampering", "Shaggy", 6197795656);
+    petSalon.pets.push(pet1, pet2);
+    
+}
+window.onload=init;
 
 
 
